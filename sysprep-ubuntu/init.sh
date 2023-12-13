7e9e87efcf8c60d9ed92c0fccace65f3b3433857
Hong Yan <homer.yan@gmail.com>
2023-12-13 01:16:05 -0700

    chore: add containerd config file modification

--- sysprep-ubuntu/init.sh
+++ sysprep-ubuntu/init.sh
@@ -110,4 +110,7 @@ Prep_K8S_Env() {
   # Configure containerd and start service
   sudo mkdir -p /etc/containerd
   sudo containerd config default | sudo tee /etc/containerd/config.toml
+
+  sudo sed -i 's/sandbox_image = "registry.k8s.io\/pause:3.6"/sandbox_image = "registry.k8s.io\/pause:3.9"/' /etc/containerd/config.toml
+  sudo sed -i 's/SystemdCgroup = false/SystemdCgroup = true/' /etc/containerd/config.toml
 }
