40960b2b32f2b0a1d813bb3af8338a6d812836fb
Hong Yan <homer.yan@gmail.com>
2023-12-14 23:14:32 -0700

    fix: conf file generation error

--- sysprep-ubuntu/prepare-k8s-install.sh
+++ sysprep-ubuntu/prepare-k8s-install.sh
@@ -1,12 +1,28 @@
 echo "Getting ready for k8s..."
 # Forwarding IPv4 and letting iptables see bridged traffic
-echo -e "overlay\nbr_netfilter" | sudo tee -a /etc/modules-load.d/k8s.conf
+sudo rm -rf /etc/modules-load.d/k8s.conf
+cat <<EOF | sudo tee /etc/modules-load.d/k8s.conf
+overlay
+br_netfilter
+EOF
 sudo modprobe overlay
 sudo modprobe br_netfilter
 lsmod | grep overlay
 lsmod | grep br_netfilter
 
-echo -e "net.core.rmem_max=26214400\nnet.core.rmem_default=26214400\n\nnet.bridge.bridge-nf-call-ip6tables = 1\nnet.bridge.bridge-nf-call-iptables = 1\nnet.ipv4.conf.default.rp_filter=1\nnet.ipv4.conf.all.rp_filter=1\nnet.ipv4.ip_forward=1\nnet.ipv6.conf.all.forwarding=1\nnet.netfilter.nf_conntrack_max=524288" | sudo tee -a /etc/sysctl.conf
+sudo cp /etc/sysctl.conf /etc/sysctl.conf.backup
+sudo rm -rf /etc/sysctl.conf
+cat <<EOF | sudo tee -a /etc/sysctl.conf
+net.core.rmem_max=26214400
+net.core.rmem_default=26214400
+net.bridge.bridge-nf-call-ip6tables = 1
+net.bridge.bridge-nf-call-iptables = 1
+net.ipv4.conf.default.rp_filter=1
+net.ipv4.conf.all.rp_filter=1
+net.ipv4.ip_forward=1
+net.ipv6.conf.all.forwarding=1
+net.netfilter.nf_conntrack_max=524288
+EOF
 sudo sysctl -p
 sysctl net.bridge.bridge-nf-call-iptables net.bridge.bridge-nf-call-ip6tables net.ipv4.ip_forward
 
@@ -14,13 +30,11 @@ sysctl net.bridge.bridge-nf-call-iptables net.bridge.bridge-nf-call-ip6tables ne
 sudo mkdir -p /etc/containerd
 sudo containerd config default | sudo tee /etc/containerd/config.toml
 
+sudo sed -i 's/sandbox_image = "registry.k8s.io\/pause:3.6"/sandbox_image = "registry.k8s.io\/pause:3.9"/' /etc/containerd/config.toml
+sudo sed -i 's/SystemdCgroup = false/SystemdCgroup = true/' /etc/containerd/config.toml
+
 # Overwrite the sandbox(pause) image
 sudo sed -i 's/sandbox_image = "registry.k8s.io\/pause:3.6"/sandbox_image = "registry.k8s.io\/pause:3.9"/' /etc/containerd/config.toml
 # Use the SystemdCgroup driver
 sudo sed -i 's/SystemdCgroup = false/SystemdCgroup = true/' /etc/containerd/config.toml
-
-echo "\n-------------------"
-cat /etc/containerd/config.toml | grep 'pause:3'
-cat /etc/containerd/config.toml | grep 'SystemdCgroup'
-
 sudo systemctl restart containerd
