fc5a424ecff09c912e86c86068118b61e91d6c2d
Hong Yan <homer.yan@gmail.com>
2024-02-21 03:11:16 -0700

    chore: skip README in updating docker iamges

--- docker/pull-latest-app-docker-images.sh
+++ docker/pull-latest-app-docker-images.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
-INSTALLED_APPS=(`ls /apps`)
+INSTALLED_APPS=($(ls /apps))
 
-for i in ${INSTALLED_APPS[@]}
-do
-    echo 'Pulling latest images of '$i
-    cd /apps/$i && docker-compose pull
-    echo
+for i in "${INSTALLED_APPS[@]}"; do
+	if [ "$i" = "README" ]; then
+		continue
+	fi
+
+	echo "Pulling latest images of $i"
+	cd "/apps/$i" && docker-compose pull
+	echo
 done
