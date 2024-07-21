b3b65fa6a8ea2787ccf85e3289b29c8a9a8a479f
Hong Yan <contact@yanhong.ca>
2024-07-21 16:11:11 -0600

    refactor: use install-modern-utils.sh

--- installers/astronvim-ubuntu-install.sh
+++ installers/astronvim-ubuntu-install.sh
@@ -1,53 +1,42 @@
 #!/bin/bash
 
-# Install pre-built neovim binaries for Linux systems.
-curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz
-sudo rm -rf /opt/nvim
-sudo tar -C /opt -xzf nvim-linux64.tar.gz
-rm nvim-linux64.tar.gz
-
-echo 'export PATH="/opt/nvim-linux64/bin:$PATH"' >>~/.bashrc
-
-sudo apt-get install -y python3-pip python3.10-venv python3-neovim tar unzip gcc make autojump p7zip libbz2-dev wget ripgrep
-
-export FZF_VERSION=$(curl -s "https://api.github.com/repos/junegunn/fzf/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
-curl -Lo fzf.tar.gz "https://github.com/junegunn/fzf/releases/download/v${FZF_VERSION}/fzf-${FZF_VERSION}-linux_amd64.tar.gz"
-tar xvf fzf.tar.gz fzf
-sudo install fzf /usr/local/bin
-rm -f fzf*
-
-LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
-curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
-tar xf lazygit.tar.gz lazygit
-sudo install lazygit /usr/local/bin
-rm lazygit*
-
-curl -fsSL https://deb.nodesource.com/setup_20.x | sudo -E bash - &&
-	sudo apt-get install -y nodejs
-
-sudo add-apt-repository ppa:daniel-milde/gdu -y
 sudo apt-get update
-sudo apt-get install gdu -y
+sudo apt-get install -y gcc make libbz2-dev python3-pip
+bash -c "$(curl -fsSL https://raw.githubusercontent.com/hongyanca/shell-script-utilities/main/installers/install-modern-utils.sh)"
 
-# https://github.com/ClementTsang/bottom
-curl -LO https://github.com/ClementTsang/bottom/releases/download/0.9.6/bottom_0.9.6_amd64.deb
-sudo dpkg -i bottom_0.9.6_amd64.deb
-rm bottom_0.9.6_amd64.deb
-
+# Install Node.js 20.x
+curl -fsSL https://deb.nodesource.com/setup_20.x | sudo -E bash - && sudo apt-get install -y nodejs
+# Install npm packages globally without sudo on Linux
 mkdir -p "${HOME}/.npm-packages"
 npm config set prefix "${HOME}/.npm-packages"
+echo 'NPM_PACKAGES="${HOME}/.npm-packages"' >>~/.zshrc
+echo 'export PATH="$PATH:$NPM_PACKAGES/bin"' >>~/.zshrc
+echo 'export MANPATH="${MANPATH-$(manpath)}:$NPM_PACKAGES/share/man"' >>~/.zshrc
 echo 'NPM_PACKAGES="${HOME}/.npm-packages"' >>~/.bashrc
 echo 'export PATH="$PATH:$NPM_PACKAGES/bin"' >>~/.bashrc
 echo 'export MANPATH="${MANPATH-$(manpath)}:$NPM_PACKAGES/share/man"' >>~/.bashrc
-
 source ~/.bashrc
-npm install tree-sitter-cli -g
+npm install tree-sitter-cli neovim pyright -g
 
-pip3 install --user --upgrade neovim
-npm install -g neovim
+python3 -m pip install --upgrade pip --break-system-packages
+python3 -m pip install --user --upgrade pynvim --break-system-packages
 
-git clone --depth 1 https://github.com/AstroNvim/AstroNvim ~/.config/nvim
-git clone https://github.com/hongyanca/astronvim-user ~/.config/nvim/lua/user
+rm -rf ~/.local/share/nvim
+rm -rf ~/.local/state/nvim
+rm -rf ~/.cache/nvim
+rm -rf ~/.config/nvim
+git clone --depth 1 https://github.com/hongyanca/astronvim_config ~/.config/nvim
+# Comment out python and rust community packages
+sed -i 's/^\(\s*{\s*import\s*=\s*"astrocommunity\.pack\.rust"\s*}\)/  --\1/' ~/.config/nvim/lua/community.lua
+sed -i 's/^\(\s*{\s*import\s*=\s*"astrocommunity\.pack\.python"\s*}\)/  --\1/' ~/.config/nvim/lua/community.lua
+# Copy neovim runtime files
+sudo rm -rf /tmp/neovim
+git clone --depth 1 --branch v0.10.0 https://github.com/neovim/neovim /tmp/neovim
+sudo rm -rf /usr/local/share/nvim
+sudo cp -r /tmp/neovim/runtime /usr/local/share/nvim/
+# Install neovim configurations
+nvim --headless -c 'quitall'
+sudo rm -rf /tmp/neovim
 
 sudo rm -f /usr/bin/nvim
-sudo ln -s /opt/nvim-linux64/bin/nvim /usr/bin/nvim
+sudo ln -s /usr/local/bin/nvim /usr/bin/nvim
