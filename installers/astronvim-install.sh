25354803d8b748d50d84467203d6f9e1417dfe1b
Hong Yan <contact@yanhong.ca>
2024-07-22 23:12:32 -0600

    refactor: astronvim-install.sh works on rhel and debian

--- installers/astronvim-install.sh
+++ installers/astronvim-install.sh
@@ -1,11 +1,45 @@
 #!/bin/bash
 
-sudo apt-get update
-sudo apt-get install -y gcc make libbz2-dev python3-pip
+LINUX_DISTRO="unknown"
+# Function to set the LINUX_DISTRO variable based on the ID_LIKE value
+get_distro() {
+  # Read the ID_LIKE value from /etc/os-release
+  ID_LIKE=$(grep ^ID_LIKE= /etc/os-release | cut -d= -f2 | tr -d '"')
+
+  # Check if ID_LIKE contains "rhel" or "debian"
+  if [[ $ID_LIKE == *"rhel"* ]]; then
+    LINUX_DISTRO="rhel"
+  elif [[ $ID_LIKE == *"debian"* ]]; then
+    LINUX_DISTRO="debian"
+  else
+    LINUX_DISTRO="unknown"
+  fi
+}
+get_distro
+
+# Install packages based on the LINUX_DISTRO value
+if [[ $LINUX_DISTRO == "rhel" ]]; then
+  sudo dnf upgrade --refresh -y
+  sudo dnf install -y yum-utils gcc make python3-pip
+  python3 -m pip install --upgrade pip
+  python3 -m pip install --user --upgrade pynvim
+  # Install Node.js 20.x
+  # Use `sudo dnf module list nodejs` to list available Node.js versions
+  sudo dnf module install nodejs:20/common
+elif [[ $LINUX_DISTRO == "debian" ]]; then
+  sudo apt-get update
+  sudo apt-get install -y gcc make libbz2-dev python3-pip
+  python3 -m pip install --upgrade pip --break-system-packages
+  python3 -m pip install --user --upgrade pynvim --break-system-packages
+  # Install Node.js 20.x
+  curl -fsSL https://deb.nodesource.com/setup_20.x | sudo -E bash - && sudo apt-get install -y nodejs
+else
+  echo "Unknown distro" >&2
+  exit 1
+fi
+
 bash -c "$(curl -fsSL https://raw.githubusercontent.com/hongyanca/shell-script-utilities/main/installers/install-modern-utils.sh)"
 
-# Install Node.js 20.x
-curl -fsSL https://deb.nodesource.com/setup_20.x | sudo -E bash - && sudo apt-get install -y nodejs
 # Install npm packages globally without sudo on Linux
 mkdir -p "${HOME}/.npm-packages"
 npm config set prefix "${HOME}/.npm-packages"
@@ -15,11 +49,8 @@ echo 'export MANPATH="${MANPATH-$(manpath)}:$NPM_PACKAGES/share/man"' >>~/.zshrc
 echo 'NPM_PACKAGES="${HOME}/.npm-packages"' >>~/.bashrc
 echo 'export PATH="$PATH:$NPM_PACKAGES/bin"' >>~/.bashrc
 echo 'export MANPATH="${MANPATH-$(manpath)}:$NPM_PACKAGES/share/man"' >>~/.bashrc
-source ~/.bashrc
+source $HOME/.bashrc
 npm install tree-sitter-cli neovim pyright -g
-
-python3 -m pip install --upgrade pip --break-system-packages
-python3 -m pip install --user --upgrade pynvim --break-system-packages
 
 rm -rf ~/.local/share/nvim
 rm -rf ~/.local/state/nvim
