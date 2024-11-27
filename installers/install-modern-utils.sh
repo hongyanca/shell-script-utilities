988295cdf286aca6f52af4934b817ae0ae9c2223
Hong Yan <contact@yanhong.ca>
2024-11-26 22:22:47 -0700

    feat: add xclip installation

--- installers/install-modern-utils.sh
+++ installers/install-modern-utils.sh
@@ -46,7 +46,7 @@ install_required_package() {
 }
 
 echo "Installing required packages..."
-REQUIRED_PKGS=("wget" "curl" "zsh" "tar" "jq" "unzip" "p7zip" "bzip2" "make" "autojump" "git")
+REQUIRED_PKGS=("wget" "curl" "zsh" "tar" "jq" "unzip" "p7zip" "bzip2" "make" "autojump" "git" "xclip")
 # Install each package in the packages array
 for package in "${REQUIRED_PKGS[@]}"; do
   install_required_package "$package"
